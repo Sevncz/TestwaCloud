0e11eafdd0455a8732039cfef53c4be99e06df6b
Sevncz <wencz0321@gmail.com>
2017-07-29 20:35:10 +0800

    区分token过期和非法

--- distest-web/src/main/java/com/testwa/distest/server/security/JwtAuthenticationEntryPoint.java
+++ distest-web/src/main/java/com/testwa/distest/server/security/JwtAuthenticationEntryPoint.java
@@ -4,6 +4,10 @@ import com.alibaba.fastjson.JSON;
 import com.testwa.distest.server.exception.AuthorizedException;
 import com.testwa.distest.server.mvc.beans.Result;
 import com.testwa.distest.server.mvc.beans.ResultCode;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -15,14 +19,33 @@ import java.io.IOException;
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
+    @Value("${jwt.header}")
+    private String tokenHeader;
+    @Value("${jwt.secret}")
+    private String secret;
+
     @Override
     public void commence(HttpServletRequest request,
                          HttpServletResponse response,
                          AuthenticationException authException) throws IOException {
+
         response.setCharacterEncoding("UTF-8");
         response.setContentType("application/json");
-
+        String token = request.getHeader(tokenHeader);
         Result<String> r = new Result<>();
+        try {
+            Jwts.parser()
+                    .setSigningKey(secret)
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (ExpiredJwtException e) {
+
+            r.setCode(ResultCode.EXPRIED_TOKEN.getValue());
+            r.setMessage("token已过期");
+            response.getWriter().println(JSON.toJSON(r));
+            response.getWriter().flush();
+            return;
+        }
         r.setCode(ResultCode.ILLEGAL_TOKEN.getValue());
         r.setMessage("非法的token");
         response.getWriter().println(JSON.toJSON(r));
