363feebe6cfb0b4f2472aeb7d1a25a3532ec0900
edwardyangxin <yx_yoyo@live.cn>
2017-09-11 23:22:25 +0800

    增加报错提示

--- distest-web/src/main/java/com/testwa/distest/server/exception/GlobalExceptionHandler.java
+++ distest-web/src/main/java/com/testwa/distest/server/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.testwa.distest.server.exception;
 
 import com.testwa.distest.server.mvc.beans.ResultCode;
 import com.testwa.distest.server.mvc.beans.Result;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,17 +11,22 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 
+@Slf4j
 @ControllerAdvice
 class GlobalExceptionHandler {
-    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
-    @ExceptionHandler(value = RuntimeException.class)
+    @ExceptionHandler(value = Exception.class)
     @ResponseBody
     public Result defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
         log.error("Unknow error", e);
         Result<String> r = new Result<>();
-        r.setCode(ResultCode.SERVER_ERROR.getValue());
-        r.setData("Some error in server.");
+        if (e instanceof RuntimeException) {
+            r.setCode(ResultCode.SERVER_ERROR.getValue());
+            r.setData("Some error in server.");
+        } else {
+            r.setCode(ResultCode.INVALID_PARAM.getValue());
+            r.setData(e.getClass().getCanonicalName());
+        }
         r.setMessage(e.getMessage());
         r.setUrl(req.getRequestURL().toString());
 
