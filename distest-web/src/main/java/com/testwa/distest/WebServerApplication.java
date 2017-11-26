b577b3108d57794b868e650ca1391951be5e2040
Sevncz <wencz0321@gmail.com>
2017-11-26 20:03:31 +0800

    增加服务发现

--- distest-web/src/main/java/com/testwa/distest/WebServerApplication.java
+++ distest-web/src/main/java/com/testwa/distest/WebServerApplication.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.mongodb.MongoDbFactory;
@@ -26,7 +27,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import java.net.UnknownHostException;
 import java.util.concurrent.Executor;
 
-
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableMongoRepositories(repositoryBaseClass = CommonMongoRepositoryImpl.class, basePackages = {"com.testwa.distest.server.mvc.repository"})
 @EnableScheduling
