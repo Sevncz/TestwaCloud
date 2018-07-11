c3ac33bc6cb24af897dd4f425f0ee8e3f3f8da38
wen <wencz0321@gmail.com>
2018-07-11 10:17:24 +0800

    测试过程录制

--- distest-client/src/main/java/com/testwa/distest/client/component/Constant.java
+++ distest-client/src/main/java/com/testwa/distest/client/component/Constant.java
@@ -4,27 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 @Slf4j
 public class Constant {
-
-    // CPU架构的种类
-    public static final String ABIS_ARM64_V8A = "arm64-v8a";
-    public static final String ABIS_ARMEABI_V7A = "armeabi-v7a";
-    public static final String ABIS_X86 = "x86";
-    public static final String ABIS_X86_64 = "x86_64";
-
-	public static final String PROP_ABI = "ro.product.cpu.abi";
-	public static final String PROP_SDK = "ro.build.version.sdk";
-    public static final String PROP_REL = "ro.build.version.release";
-
-    public static final String MINICAP_BIN = "minicap";
-    public static final String MINICAP_SO = "minicap.so";
-    public static final String MINICAP_NOPIE = "minicap-nopie";
-    public static final String MINICAP_DIR = "/data/local/tmp/minicap-devel";
-
 
     public static final String MINITOUCH_BIN = "minitouch";
     public static final String MINITOUCH_NOPIE = "minitouch-nopie";
@@ -60,26 +45,10 @@ public class Constant {
     }
 
     public static File getTmpFile(String fileName) {
-        String tmpdir = System.getProperty("java.io.tmpdir");
-        File tmp = new File(tmpdir);
-        tmp = new File(tmp, "distest-agent");
-        if (!tmp.exists()) {
-            tmp.mkdirs();
-        }
-        return new File(tmp, fileName);
+        Path tmpPath = Paths.get(AGENT_TMP_DIR, fileName);
+        return tmpPath.toFile();
     }
 
-	public static String getMinicap() {
-		return File.separator + "minicap";
-	}
-
-	public static String getMinicapBin() {
-		return File.separator + "minicap" + File.separator + "libs";
-	}
-
-	public static String getMinicapSo() {
-		return File.separator + "minicap" + File.separator + "shared";
-	}
 
     public static String getKeyboardService() {
         return "keyboardservice" + File.separator + KEYBOARD_SERVICE_APK;
--- distest-client/src/main/java/com/testwa/distest/client/component/executor/TestTaskListener.java
+++ distest-client/src/main/java/com/testwa/distest/client/component/executor/TestTaskListener.java
@@ -1,1 +1,1 @@
-package com.testwa.distest.client.component.executor;public interface TestTaskListener {    void screen();    void click(String cmd);}
+package com.testwa.distest.client.component.executor;public interface TestTaskListener {    /**     * 测试完成     */    void taskFinish();}
