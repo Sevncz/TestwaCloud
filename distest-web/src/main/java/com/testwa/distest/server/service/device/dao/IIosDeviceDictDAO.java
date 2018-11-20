94b6bbf7e2e739a0d4e35f20133a941a9ba372e8
wen <wencz0321@gmail.com>
2018-11-20 16:10:43 +0800

    update

---
+++ distest-web/src/main/java/com/testwa/distest/server/service/device/dao/IIosDeviceDictDAO.java
@@ -0,0 +1,11 @@
+package com.testwa.distest.server.service.device.dao;
+
+import com.testwa.core.base.dao.IBaseDAO;
+import com.testwa.distest.common.enums.DB;
+import com.testwa.distest.server.entity.DeviceShareScope;
+import com.testwa.distest.server.entity.IosDeviceDict;
+
+public interface IIosDeviceDictDAO extends IBaseDAO<IosDeviceDict, Long> {
+
+    IosDeviceDict findByProductType(String productType);
+}
