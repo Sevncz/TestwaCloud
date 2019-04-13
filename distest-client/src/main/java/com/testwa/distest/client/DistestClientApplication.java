8b2a6ab5581cf997777da30feb13d12f772314b0
文雯 <wenwen@puhuilink.com>
2019-04-13 23:53:20 +0800

    grpc去掉cloud自动发现

--- distest-client/src/main/java/com/testwa/distest/client/DistestClientApplication.java
+++ distest-client/src/main/java/com/testwa/distest/client/DistestClientApplication.java
@@ -4,9 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -16,12 +13,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 @EnableScheduling
 @EnableAsync
-@EnableEurekaClient
-@EnableDiscoveryClient
 public class DistestClientApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(DistestClientApplication.class, args);
     }
 
