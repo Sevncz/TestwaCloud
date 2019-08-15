b283a6e304ce2b960d79baee4ee2a0b3bb58f4ae
文雯 <wenwen@puhuilink.com>
2019-08-15 18:02:01 +0800

    touchperform对象修复

--- distest-client/src/main/java/com/testwa/distest/client/device/driver/TouchPerformAction.java
+++ distest-client/src/main/java/com/testwa/distest/client/device/driver/TouchPerformAction.java
@@ -1,8 +1,10 @@
 package com.testwa.distest.client.device.driver;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class TouchPerformAction {
     private String action;
     private TouchPerformActionOption options;
--- distest-client/src/main/java/com/testwa/distest/client/device/driver/TouchPerformActionOption.java
+++ distest-client/src/main/java/com/testwa/distest/client/device/driver/TouchPerformActionOption.java
@@ -1,12 +1,14 @@
 package com.testwa.distest.client.device.driver;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class TouchPerformActionOption {
-
 //    private String element = "";
     private Integer x;
     private Integer y;
     private Integer count;
+    private Integer ms;
 }
